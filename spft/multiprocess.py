9984f6805e445fb975b9ce6b06fa1fcea65b0c81
hunggun <panshuai2019ps@163.com>
2022-09-01 10:38:31 +0800

    add multi process generator

--- spft/multiprocess.py
+++ spft/multiprocess.py
@@ -52,5 +52,41 @@ def multi_process(func, iterable, worker_num, is_queue=True):
         results.sort(key=lambda x: x[0])
         return [x[1] for x in results]
     else:
-        with Pool(worker_num) as pool:
-            return pool.map(func, iterable)
+        if func.__code__.co_argcount == 1:
+            with Pool(worker_num) as pool:
+                return pool.map(func, iterable)
+        else:
+            with Pool(worker_num) as pool:
+                return pool.starmap(func, iterable)
+
+
+def multi_process_generator(func, iterable, worker_num):
+    """
+    :param func: 调用的函数
+    :param iterable: 可迭代对象
+    :param worker_num: 进程数量
+    :param is_queue: 是否使用队列，否使用进程池
+    :return: 列表包括每一个任务的结果
+    """
+    # Create queues
+    task_queue = Queue()
+    done_queue = Queue()
+
+    # Submit tasks
+    for idx, task in enumerate(iterable):
+        task_queue.put((idx, func, task))
+
+    # Start worker processes
+    for i in range(worker_num):
+        if func.__code__.co_argcount == 1:
+            Process(target=worker_single, args=(task_queue, done_queue)).start()
+        else:
+            Process(target=worker, args=(task_queue, done_queue)).start()
+
+    # get result
+    for _ in tqdm(iterable):
+        yield done_queue.get()
+
+    # Tell child processes to stop
+    for i in range(worker_num):
+        task_queue.put('STOP')
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from spft.multiprocess import multi_process
+from spft.multiprocess import multi_process, multi_process_generator
 from tqdm import tqdm
+import json
+import numpy as np
 
 
 def funcs(data):
@@ -9,4 +11,6 @@ def funcs(data):
 
 
 if __name__ == '__main__':
-    multi_process(funcs, tqdm(list(zip(*[range(10000000), range(10000000)]))), 6, is_queue=False)
+    # multi_process(funcs, tqdm(list(zip(*[range(10000000), range(10000000)]))), 6, is_queue=False)
+    for data in multi_process_generator(funcs, list(zip(*[range(100), range(100)])), 6):
+        print(data)
